bb9e5b57b83a9cdaebfb1472b9e6010744abbfbf
syedatayabakazmi <tayabatabbasum1@gmail.com>
2024-04-23 03:02:15 +0500

    Update ContactbookApplicationTests.java

--- src/test/java/com/contact/contactbook/ContactbookApplicationTests.java
+++ src/test/java/com/contact/contactbook/ContactbookApplicationTests.java
@@ -1,10 +1,10 @@
-package com.contactmanager.smartcontactmanager;
+package com.contact.contactbook;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SmartcontactmanagerApplicationTests {
+class ContactbookApplicationTests {
 
 	@Test
 	void contextLoads() {
